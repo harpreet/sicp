2a81ead1a834722e2c0dc00eabe37a8ea076fe40
Ramakrishnan Muthukrishnan <vu3rdd@gmail.com>
2010-05-27 11:07:22 +0530

    adding leiningen project file

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject sicp "1.0.0-SNAPSHOT"
+  :description "SICP experiments and exercise solutions"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])

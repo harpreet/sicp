6e3abd18b3591d61bd5659e5d1e01608d991eceb
Ramakrishnan Muthukrishnan <vu3rdd@gmail.com>
2010-06-14 16:12:11 +0530

    solution to 2.16

---
+++ src/sicp/ex2_16.clj
@@ -0,0 +1,9 @@
+"This is already explained partly in 2.15. Any operation like
+A = A^3/A^2, will yield a different interval than the original A.
+
+How do we fix this? One way is to do algebraic reduction of the
+expression before doing any interval arithmetic.
+
+Are there some better ways? I am very interested to hear. The
+usual literature (wikipedia et al) seem to be mum on these
+important issues."

08a3aa8fadf49910c525d42da9e566bbeb75fae7
Ramakrishnan Muthukrishnan <vu3rdd@gmail.com>
2010-06-14 16:12:24 +0530

    solution to 2.15.

--- src/sicp/ex2_15.clj
+++ src/sicp/ex2_15.clj
@@ -2,8 +2,8 @@
   (:use [sicp utils ch2_1_extended ex2_7 ex2_8 ex2_12]
 	[clojure.test]))
 
-(def r1 (make-center-percent 1000 0.01))
-(def r2 (make-center-percent 100  0.01))
+(def r1 (make-center-percent 1000 1))
+(def r2 (make-center-percent 100  5))
 
 (def r3 (sub-interval (add-interval r1 r2) r2))
 ;;=> (999.8799999999999 1000.1200000000001)
@@ -27,6 +27,5 @@ Now r1+r2-r2 = c1 + c2 - c1 +/- (2*w1+w2)
 
 i.e. we have a new percentage width as the errors add up. The inference
 from this is that, we reduce the number of ranges in a calculation to
-get a more accurate range. So, yes, Eva Lu Ator is right.
-"
-  )
+get a more accurate range. So, yes, Eva Lu Ator is right."
+)
